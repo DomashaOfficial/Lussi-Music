5c355a1aa290a47caa16c35f57bbae0f9794c9a2
DOMASHA RASHMINDU <97603049+DomashaOfficial@users.noreply.github.com>
2022-03-09 16:18:02 +0530

    Update __init__.py

--- LussiMusic/__init__.py
+++ LussiMusic/__init__.py
@@ -1,17 +1,17 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2022 by DomashaOfficial@Github, < https://github.com/DomashaOfficial >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/DomashaOfficial/Lussi-Music > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/DomashaOfficial/Lussi-Music/blob/master/LICENSE >
 #
 # All rights reserved.
 
-from YukkiMusic.core.bot import YukkiBot
-from YukkiMusic.core.dir import dirr
-from YukkiMusic.core.git import git
-from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku, sudo
+from LussiMusic.core.bot import LussiBot
+from LussiMusic.core.dir import dirr
+from LussiMusic.core.git import git
+from LussiMusic.core.userbot import Userbot
+from LussiMusic.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
